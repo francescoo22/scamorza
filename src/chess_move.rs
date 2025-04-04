da57f073903fdd4b79ccd2c5009af1ef0ce22cd2
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-04-04 17:03:40 +0200

    Fix UCI moves

--- src/chess_move.rs
+++ src/chess_move.rs
@@ -13,9 +13,9 @@ impl Move {
         }
 
         let bytes = s.as_bytes();
-        let from_col = (bytes[0] - b'a') as usize;
+        let from_col = (b'h' - bytes[0]) as usize;
         let from_row = (bytes[1] - b'1') as usize;
-        let to_col = (bytes[2] - b'a') as usize;
+        let to_col = (b'h' - bytes[2]) as usize;
         let to_row = (bytes[3] - b'1') as usize;
         Move {
             from: (from_row, from_col),
@@ -25,9 +25,9 @@ impl Move {
 
     pub fn to_uci_string(&self) -> String {
         let from_row = char::from(self.from.0 as u8 + b'1');
-        let from_col = char::from(self.from.1 as u8 + b'a');
+        let from_col = char::from(b'h' - self.from.1 as u8);
         let to_row = char::from(self.to.0 as u8 + b'1');
-        let to_col = char::from(self.to.1 as u8 + b'a');
+        let to_col = char::from(b'h' - self.to.1 as u8);
         format!("{}{}{}{}", from_col, from_row, to_col, to_row)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ fn main() {
     let mut board = chess_board::ChessBoard::initial_board();
     println!("{}", board);
     board.all_valid_moves();
-    board.move_piece("d2d4");
+    board.move_piece("e2e4");
     println!("{}", board);
     board.all_valid_moves();
-    board.move_piece("e7e5");
+    board.move_piece("c7c6");
     println!("{}", board);
     board.all_valid_moves();
     board.move_piece("a2a4");
     println!("{}", board);
     board.all_valid_moves();
-    board.move_piece("e7e5");
+    board.move_piece("d7d5");
     println!("{}", board);
     board.all_valid_moves();
 }
