2089ea3179aec26eaad0199460e7ea9d5b53315e
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-04-04 16:51:35 +0200

    Add simple valid moves

--- src/chess_board.rs
+++ src/chess_board.rs
@@ -56,7 +56,7 @@ impl Piece {
 }
 
 impl fmt::Display for Piece {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let symbol = match (self.color, self.kind) {
             (Color::White, PieceKind::Pawn) => "♙",
             (Color::White, PieceKind::Rook) => "♖",
@@ -84,7 +84,7 @@ enum Square {
 }
 
 impl fmt::Display for Square {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Square::Empty => write!(f, "-"),
             Square::Occupied(piece) => write!(f, "{}", piece),
@@ -95,6 +95,10 @@ impl fmt::Display for Square {
 #[derive(Clone, Copy)]
 pub struct ChessBoard {
     squares: [[Square; 8]; 8],
+}
+
+fn within_bounds(i: i32, j: i32) -> bool {
+    i < 8 && j < 8 && i >= 0 && j >= 0
 }
 
 impl fmt::Display for ChessBoard {
@@ -168,11 +172,11 @@ impl ChessBoard {
 
     fn for_each_piece<F>(&self, mut block: F)
     where
-        F: FnMut(usize, usize, &Piece),
+        F: FnMut(i32, i32, &Piece),
     {
         for i in 0..8 {
             for j in 0..8 {
-                match self.squares[i][j] {
+                match self.squares[i as usize][j as usize] {
                     Square::Empty => {}
                     Square::Occupied(piece) => {
                         block(i, j, &piece);
@@ -182,71 +186,172 @@ impl ChessBoard {
         }
     }
 
-    fn pawn_valid_moves(&self, i: usize, j: usize, color: &Color) -> Vec<Move> {
+    fn within_bounds_and_empty(&self, i: i32, j: i32) -> bool {
+        within_bounds(i, j) && self.squares[i as usize][j as usize] == Square::Empty
+    }
+
+    fn within_bounds_and_occupied_by_opponent(&self, i: i32, j: i32, color: &Color) -> bool {
+        if !within_bounds(i, j) {
+            return false;
+        }
+        match self.squares[i as usize][j as usize] {
+            Square::Empty => false,
+            Square::Occupied(piece) => piece.color != *color,
+        }
+    }
+
+    fn slider_valid_moves(
+        &self,
+        i: i32,
+        j: i32,
+        color: &Color,
+        directions: &Vec<(i32, i32)>,
+    ) -> Vec<Move> {
         let mut moves = Vec::new();
-        match color {
-            Color::White => {
-                if self.squares[i + 1][j] == Square::Empty {
-                    moves.push(Move {
-                        from: (i, j),
-                        to: (i + 1, j),
-                    });
-                    if i == 1 && self.squares[i + 2][j] == Square::Empty {
-                        moves.push(Move {
-                            from: (i, j),
-                            to: (i + 2, j),
-                        });
-                    }
-                }
-                if j < 7 {
-                    match self.squares[i + 1][j + 1] {
-                        Square::Occupied(other_piece) => {
-                            if other_piece.color != *color {
-                                moves.push(Move {
-                                    from: (i, j),
-                                    to: (i + 1, j + 1),
-                                });
-                            }
-                        }
-                        Square::Empty => {}
-                    }
-                }
-                if j > 0 {
-                    match self.squares[i + 1][j - 1] {
-                        Square::Occupied(other_piece) => {
-                            if other_piece.color != *color {
-                                moves.push(Move {
-                                    from: (i, j),
-                                    to: (i + 1, j - 1),
-                                });
-                            }
-                        }
-                        Square::Empty => {}
-                    }
-                }
+        for (dx, dy) in directions {
+            let mut dist = 1;
+            while self.within_bounds_and_empty(i + dist * dy, j + dist * dx) {
+                dist += 1;
             }
-            Color::Black => {
-                todo!()
+            for cur_dist in 1..dist {
+                moves.push(Move {
+                    from: (i as usize, j as usize),
+                    to: ((i + cur_dist * dy) as usize, (j + cur_dist * dx) as usize),
+                });
             }
+            if self.within_bounds_and_occupied_by_opponent(i + dist * dy, j + dist * dx, color) {
+                moves.push(Move {
+                    from: (i as usize, j as usize),
+                    to: ((i + dist * dy) as usize, (j + dist * dx) as usize),
+                });
+            }
+        }
+
+        moves
+    }
+
+    fn leaper_valid_moves(
+        &self,
+        i: i32,
+        j: i32,
+        color: &Color,
+        directions: &Vec<(i32, i32)>,
+    ) -> Vec<Move> {
+        let mut moves = Vec::new();
+
+        for (dx, dy) in directions {
+            if self.within_bounds_and_empty(i + dy, j + dx)
+                || self.within_bounds_and_occupied_by_opponent(i + dy, j + dx, color)
+            {
+                moves.push(Move {
+                    from: (i as usize, j as usize),
+                    to: ((i + dy) as usize, (j + dx) as usize),
+                });
+            }
+        }
+
+        moves
+    }
+
+    fn knight_valid_moves(&self, i: i32, j: i32, color: &Color) -> Vec<Move> {
+        let dirs = vec![
+            (2, 1),
+            (1, 2),
+            (-1, 2),
+            (-2, 1),
+            (-2, -1),
+            (-1, -2),
+            (1, -2),
+            (2, -1),
+        ];
+        self.leaper_valid_moves(i, j, color, &dirs)
+    }
+
+    fn king_valid_moves(&self, i: i32, j: i32, color: &Color) -> Vec<Move> {
+        let dirs = vec![
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1),
+            (1, 1),
+            (1, -1),
+            (-1, 1),
+            (-1, -1),
+        ];
+        self.leaper_valid_moves(i, j, color, &dirs)
+    }
+
+    fn rook_valid_moves(&self, i: i32, j: i32, color: &Color) -> Vec<Move> {
+        let dirs = vec![(1, 0), (0, 1), (-1, 0), (0, -1)];
+        self.slider_valid_moves(i, j, color, &dirs)
+    }
+
+    fn bishop_valid_moves(&self, i: i32, j: i32, color: &Color) -> Vec<Move> {
+        let dirs = vec![(1, 1), (1, -1), (-1, 1), (-1, -1)];
+        self.slider_valid_moves(i, j, color, &dirs)
+    }
+
+    fn queen_valid_moves(&self, i: i32, j: i32, color: &Color) -> Vec<Move> {
+        let dirs = vec![
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1),
+            (1, 1),
+            (1, -1),
+            (-1, 1),
+            (-1, -1),
+        ];
+        self.slider_valid_moves(i, j, color, &dirs)
+    }
+
+    fn pawn_valid_moves(&self, i: i32, j: i32, color: &Color) -> Vec<Move> {
+        let mut moves = Vec::new();
+        let dir = match color {
+            Color::White => 1,
+            Color::Black => -1,
+        };
+        if self.within_bounds_and_empty(i + dir, j) {
+            moves.push(Move {
+                from: (i as usize, j as usize),
+                to: ((i + dir) as usize, j as usize),
+            });
+            if self.within_bounds_and_empty(i + 2 * dir, j) {
+                moves.push(Move {
+                    from: (i as usize, j as usize),
+                    to: ((i + 2 * dir) as usize, j as usize),
+                });
+            }
+        }
+        if self.within_bounds_and_occupied_by_opponent(i + dir, j + 1, color) {
+            moves.push(Move {
+                from: (i as usize, j as usize),
+                to: ((i + dir) as usize, (j + 1) as usize),
+            });
+        }
+        if self.within_bounds_and_occupied_by_opponent(i + dir, j - 1, color) {
+            moves.push(Move {
+                from: (i as usize, j as usize),
+                to: ((i + dir) as usize, (j - 1) as usize),
+            });
         }
         moves
     }
 
-    fn piece_valid_moves(&self, i: usize, j: usize, piece: &Piece) -> Vec<Move> {
+    fn piece_valid_moves(&self, i: i32, j: i32, piece: &Piece) -> Vec<Move> {
         match piece.kind {
             PieceKind::Pawn => self.pawn_valid_moves(i, j, &piece.color),
-            _ => Vec::new(), // PieceKind::Rook => { Vec::new() }
-            // PieceKind::Knight => {}
-            // PieceKind::Bishop => {}
-            // PieceKind::Queen => {}
-            // PieceKind::King => {}
+            PieceKind::Rook => self.rook_valid_moves(i, j, &piece.color),
+            PieceKind::Bishop => self.bishop_valid_moves(i, j, &piece.color),
+            PieceKind::Queen => self.queen_valid_moves(i, j, &piece.color),
+            PieceKind::Knight => self.knight_valid_moves(i, j, &piece.color),
+            PieceKind::King => self.king_valid_moves(i, j, &piece.color),
         }
     }
 
-    pub fn all_valid_moves(&self, move_color: Color) -> Vec<Move> {
+    pub fn all_valid_moves(&self) -> Vec<Move> {
         let mut moves = Vec::new();
         self.for_each_piece(|i, j, piece| {
-            if piece.color == move_color {
                 let valid_moves = self.piece_valid_moves(i, j, piece);
                 println!(
                     "{}: {}",
@@ -258,7 +363,6 @@ impl ChessBoard {
                         .join(" ")
                 );
                 moves.extend(valid_moves);
-            }
         });
         moves
     }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use crate::chess_board::Color;
-
 mod chess_board;
 mod chess_move;
 
 fn main() {
     let mut board = chess_board::ChessBoard::initial_board();
     println!("{}", board);
-    board.all_valid_moves(Color::White);
+    board.all_valid_moves();
     board.move_piece("d2d4");
     println!("{}", board);
-    board.all_valid_moves(Color::White);
+    board.all_valid_moves();
     board.move_piece("e7e5");
     println!("{}", board);
-    board.all_valid_moves(Color::White);
+    board.all_valid_moves();
+    board.move_piece("a2a4");
+    println!("{}", board);
+    board.all_valid_moves();
+    board.move_piece("e7e5");
+    println!("{}", board);
+    board.all_valid_moves();
 }
