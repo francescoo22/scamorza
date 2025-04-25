ea3967be05a002ce4c902deef0053b2ff58717ed
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-04-25 22:34:03 +0200

    Add more perft tests

--- tests/perft.rs
+++ tests/perft.rs
@@ -38,7 +38,6 @@ mod perft {
         }
 
         #[test]
-        #[ignore = "Slow test"]
         fn test_initial_position_depth_4() {
             assert_eq!(perft(4, &mut ChessBoard::default(), Color::White), 197281);
         }
@@ -79,6 +78,19 @@ mod perft {
                 .unwrap();
             assert_eq!(perft(3, &chess_board, chess_board.side_to_move()), 97862);
         }
+
+        // TODO: run once (~6 min) and failed, investigate
+        // Expected :4085603
+        // Actual   :4085718
+        #[test]
+        #[ignore = "Slow test"]
+        fn test_position_2_depth_4() {
+            let chess_board = ChessBoard::from_str(
+                "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -  ",
+            )
+                .unwrap();
+            assert_eq!(perft(4, &chess_board, chess_board.side_to_move()), 4085603);
+        }
     }
 
     mod position_3 {
@@ -101,7 +113,6 @@ mod perft {
         fn test_position_3_depth_3() {
             let chess_board =
                 ChessBoard::from_str("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1").unwrap();
-            // todo: add en-passant
             assert_eq!(perft(3, &chess_board, chess_board.side_to_move()), 2812);
         }
 
@@ -130,7 +141,6 @@ mod perft {
                 "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
             )
                 .unwrap();
-            // todo: all possible promotions
             assert_eq!(perft(2, &chess_board, chess_board.side_to_move()), 264);
         }
 
@@ -144,13 +154,137 @@ mod perft {
         }
 
         #[test]
-        #[ignore = "Slow test"]
         fn test_position_4_depth_4() {
             let chess_board = ChessBoard::from_str(
                 "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
             )
                 .unwrap();
             assert_eq!(perft(4, &chess_board, chess_board.side_to_move()), 422333);
+        }
+    }
+
+    mod position_4_mirrored {
+        use super::*;
+        #[test]
+        fn test_position_4_depth_1() {
+            let chess_board = ChessBoard::from_str(
+                "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+            )
+                .unwrap();
+            assert_eq!(perft(1, &chess_board, chess_board.side_to_move()), 6);
+        }
+
+        #[test]
+        fn test_position_4_depth_2() {
+            let chess_board = ChessBoard::from_str(
+                "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+            )
+                .unwrap();
+            assert_eq!(perft(2, &chess_board, chess_board.side_to_move()), 264);
+        }
+
+        #[test]
+        fn test_position_4_depth_3() {
+            let chess_board = ChessBoard::from_str(
+                "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+            )
+                .unwrap();
+            assert_eq!(perft(3, &chess_board, chess_board.side_to_move()), 9467);
+        }
+
+        #[test]
+        fn test_position_4_depth_4() {
+            let chess_board = ChessBoard::from_str(
+                "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+            )
+                .unwrap();
+            assert_eq!(perft(4, &chess_board, chess_board.side_to_move()), 422333);
+        }
+    }
+
+    mod position_5 {
+        use super::*;
+
+        #[test]
+        fn test_position_5_depth_1() {
+            let chess_board = ChessBoard::from_str(
+                "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            )
+                .unwrap();
+            assert_eq!(perft(1, &chess_board, chess_board.side_to_move()), 44);
+        }
+
+        #[test]
+        fn test_position_5_depth_2() {
+            let chess_board = ChessBoard::from_str(
+                "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            )
+                .unwrap();
+            assert_eq!(perft(2, &chess_board, chess_board.side_to_move()), 1486);
+        }
+
+
+        // TODO: investigate:
+        // actual: 62416
+        // expected: 62379
+        #[test]
+        fn test_position_5_depth_3() {
+            let chess_board = ChessBoard::from_str(
+                "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            )
+                .unwrap();
+            assert_eq!(perft(3, &chess_board, chess_board.side_to_move()), 62379);
+        }
+
+        #[test]
+        #[ignore = "Slow test"]
+        fn test_position_5_depth_4() {
+            let chess_board = ChessBoard::from_str(
+                "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            )
+                .unwrap();
+            assert_eq!(perft(4, &chess_board, chess_board.side_to_move()), 2103487);
+        }
+    }
+
+    mod position_6 {
+        use super::*;
+
+        #[test]
+        fn test_position_6_depth_1() {
+            let chess_board = ChessBoard::from_str(
+                "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            )
+                .unwrap();
+            assert_eq!(perft(1, &chess_board, chess_board.side_to_move()), 46);
+        }
+
+        #[test]
+        fn test_position_6_depth_2() {
+            let chess_board = ChessBoard::from_str(
+                "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            )
+                .unwrap();
+            assert_eq!(perft(2, &chess_board, chess_board.side_to_move()), 2079);
+        }
+
+        #[test]
+        fn test_position_6_depth_3() {
+            let chess_board = ChessBoard::from_str(
+                "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            )
+                .unwrap();
+            assert_eq!(perft(3, &chess_board, chess_board.side_to_move()), 89890);
+        }
+
+        #[test]
+        #[ignore = "Slow test"]
+        fn test_position_6_depth_4() {
+            let chess_board = ChessBoard::from_str(
+                "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            )
+                .unwrap();
+            assert_eq!(perft(4, &chess_board, chess_board.side_to_move()), 3894594);
         }
     }
 }
