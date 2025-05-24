84643cf47d2d3150e352235e18b3ac54b7f8f523
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-05-24 18:25:16 +0200

    Optimize `find_king`

--- src/check_mate.rs
+++ src/check_mate.rs
@@ -3,18 +3,13 @@ use crate::chess_piece::*;
 
 impl ChessBoard {
     fn find_king(self, color: Color) -> (i32, i32) {
-        let to_find = Square::Occupied(Piece {
-            kind: PieceKind::King,
-            color,
-        });
-        for i in 0..8 {
-            for j in 0..8 {
-                if self.at(i, j) == to_find {
-                    return (i as i32, j as i32);
-                }
-            }
-        }
-        panic!("King not found on the chess board");
+        let king_bit_board = match color {
+            Color::White => self.white_pieces & self.kings,
+            Color::Black => self.black_pieces & self.kings,
+        };
+        let king_position = king_bit_board.trailing_zeros();
+        // TODO: also here '7 - (king_position % 8)' should be changed after getting rid of old representation in move generation
+        ((king_position / 8) as i32, 7 - (king_position % 8) as i32)
     }
 
     fn is_square_checked_by_knight(self, i: i32, j: i32, color: Color) -> bool {
